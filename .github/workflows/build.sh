6b62dc120100763b93c51c36a13b07f92be0bb1c
amesgen <amesgen@amesgen.de>
2020-07-29 22:01:41 +0200

    no release assets for now

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -6,3 +6,7 @@ cabal configure -O2 --ghc-options='-split-sections -fPIC' --enable-executable-st
 INSTALL_ARGS='--installdir=artifacts --install-method=copy --overwrite-policy=always'
 
 cabal install exe:haskell-language-server $INSTALL_ARGS
+
+if [ $GHC_VERSION = '8.10.1' ]; then
+    cabal install exe:haskell-language-server-wrapper $INSTALL_ARGS
+fi
