7b739a6b993a210d5f9be7a8542a84aa41b5d363
amesgen <amesgen@amesgen.de>
2020-07-29 20:41:45 +0200

    moar stuff

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,4 +1,7 @@
+set -e
+
 cabal update
+
 cabal install exe:haskell-language-server \
-      -O2 --ghc-options='-static -optl-static -optl-pthread -fPIC' \
+      -O2 --ghc-options='-optl-static -optl-pthread -fPIC' \
       --installdir=artifacts --install-method=copy --overwrite-policy=always
