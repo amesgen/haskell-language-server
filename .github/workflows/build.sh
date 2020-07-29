3e88571cfa42482f89c82cf0cd11f7b0e1fd8416
amesgen <amesgen@amesgen.de>
2020-07-29 21:25:58 +0200

    fix derp

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,6 +3,6 @@ set -e
 cabal update
 cabal configure -O2 --ghc-options='-split-sections -fPIC' --enable-executable-static
 
-INSTALL_ARGS=--installdir=artifacts --install-method=copy --overwrite-policy=always
+INSTALL_ARGS='--installdir=artifacts --install-method=copy --overwrite-policy=always'
 
 cabal install exe:haskell-language-server $INSTALL_ARGS
