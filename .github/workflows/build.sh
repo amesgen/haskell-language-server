157e356cd29f0b70ed955efad0b1fde0438967fc
amesgen <amesgen@amesgen.de>
2020-07-29 20:24:04 +0200

    disable split sections

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,4 +1,4 @@
 cabal update
 cabal install exe:haskell-language-server \
-      -O2 --ghc-options='-split-sections -static -optl-static -optl-pthread -fPIC' \
+      -O2 --ghc-options='-static -optl-static -optl-pthread -fPIC' \
       --installdir=artifacts --install-method=copy --overwrite-policy=always
